b02d694f7f442803ff83e172517329372b4abc59
Riceball LEE <snowyu.lee@gmail.com>
2020-01-15 20:33:37 +0800

    fix: IPropDescriptor.type ts declaration miss Function

--- src/abstract.d.ts
+++ src/abstract.d.ts
@@ -9,7 +9,7 @@ export interface ISimplePropDescriptor {
 
 export interface IPropDescriptor extends ISimplePropDescriptor {
   name?: string;
-  type?: string|(string|Function)[];
+  type?: string|Function|(string|Function)[];
   assign?: (value?, dest?, src?, name?)=>any;
   assigned?: boolean|string;
   exported?: boolean;
