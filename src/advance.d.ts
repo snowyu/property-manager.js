7e60da4aa61819cf6ce0bd62461ffc90e7d244ff
Riceball LEE <snowyu.lee@gmail.com>
2020-01-16 21:20:33 +0800

    build: update typescript declaration files

--- src/advance.d.ts
+++ src/advance.d.ts
@@ -1,5 +1,12 @@
 import AbstractPropertyManager, { PropDescriptors } from './abstract';
+import Properties from './properties';
 
 export default class AdvancePropertyManager extends AbstractPropertyManager {
+  $attributes: Properties;
+
+  /**
+   * get all properties descriptor include inherited.
+   */
+  static getProperties(): PropDescriptors;
   static defineProperties(aTarget, aProperties: PropDescriptors, recreate?:boolean)
 }
--- src/normal.d.ts
+++ src/normal.d.ts
@@ -1,5 +1,12 @@
 import AbstractPropertyManager, { PropDescriptors } from './abstract';
 
 export default class NormalPropertyManager extends AbstractPropertyManager {
+  $attributes: PropDescriptors;
+
+  /**
+   * get all properties descriptor include inherited.
+   */
+  static getProperties(): PropDescriptors;
+
   static defineProperties(aTarget, aProperties:PropDescriptors, recreate?:boolean)
 }
