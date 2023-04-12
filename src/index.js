a362a17adc50dd137bce0458fa7b25b247969947
Riceball LEE <snowyu.lee@gmail.com>
2023-04-12 21:10:14 +0800

    fix: export Properties etc to index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import {NormalPropertyManager} from './normal.js'
 export {AdvancePropertyManager} from './advance.js';
 export {NormalPropertyManager} from './normal.js'
 export {SimplePropertyManager} from './simple.js';
+export {Properties} from './properties/index.js'
 export {PropertyAbility} from './ability.js'
 export {assignValue} from './assign-value.js'
 export * from './array.js'
+export {defineProperties} from './properties/define-properties.js'
 
 export default NormalPropertyManager
