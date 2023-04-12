9d31e7947ee938664103c7871a5a664ddc9b25dd
Riceball LEE <snowyu.lee@gmail.com>
2023-04-12 20:42:31 +0800

    feat(ts): export all on index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import {NormalPropertyManager} from './normal.js'
 
+export {AdvancePropertyManager} from './advance.js';
 export {NormalPropertyManager} from './normal.js'
+export {SimplePropertyManger} from './simple.js';
+export {PropertyAbility} from './ability.js'
+
 export default NormalPropertyManager
 
 
